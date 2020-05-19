74a548ff288f727dc7c7e0a0db8ee4d05325d90c
panbaodong <panbaodongpbd@163.com>
2020-05-19 11:25:27 +0800

    master分支增加helloword4

---
+++ HelloWord/src/main/java/org/pan/HelloWord4.java
@@ -0,0 +1,17 @@
+package org.pan;
+
+public class HelloWord4 {
+	public String sayHello(String str){
+		str="这个是aaa分支新增的一行";
+		return "hello aaaa111111111abbb"+str;
+	}
+	//master分支的main函数
+	public static void main(String[] argsaaaaaa){
+		
+	}
+	//aaa分支的dataDeal函数
+	private void dateDeal() {
+		
+	}
+
+}
